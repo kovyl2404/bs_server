86a1a99c196ba6fb171e2a2b0762b3402effe034
Viacheslav Kovalev <kovyl2404@gmail.com>
2014-07-21 18:45:04 +0400

    Integration tests in progress

---
+++ apps/inttests/src/client_emulator.erl
@@ -0,0 +1,219 @@
+
+-module(client_emulator).
+-author("Viacheslav V. Kovalev").
+
+-behaviour(gen_server).
+-include_lib("game_server/include/client_protocol.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+%% API
+-export([
+    start/2,
+    stop/1,
+    start_game/1,
+    stop_game/1,
+    surrender/1,
+    make_turn/2,
+    set_owner/2
+]).
+
+%% gen_server callbacks
+-export([init/1,
+    handle_call/3,
+    handle_cast/2,
+    handle_info/2,
+    terminate/2,
+    code_change/3]).
+
+-define(SERVER, ?MODULE).
+
+-record(
+    state, {
+        socket,
+        current_game,
+        parser,
+        owner
+    }
+).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+start(Host, Port) ->
+    gen_server:start(?MODULE, {Host, Port, self()}, []).
+
+stop(Pid) ->
+    gen_server:cast(Pid, stop).
+
+start_game(ClientEmulator) ->
+    ok = gen_server:cast(ClientEmulator, start_game).
+
+stop_game(ClientEmulator) ->
+    ok = gen_server:cast(ClientEmulator, stop_game).
+
+surrender(ClientEmulator) ->
+    ok = gen_server:cast(ClientEmulator, surrender).
+
+make_turn(ClientEmulator, TurnData) ->
+    ok = gen_server:cast(ClientEmulator, {make_turn, TurnData}).
+
+set_owner(ClientEmulator, Pid) ->
+    gen_server:call(ClientEmulator, {set_owner, Pid}).
+
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+
+init({Host, Port, Owner}) ->
+    {ok, Socket} = gen_tcp:connect(Host, Port, [{active, true}, binary]),
+    {ok, #state{
+        socket = Socket,
+        current_game = undefined,
+        parser = protocol_parser:init(),
+        owner = Owner
+    }}.
+
+
+handle_call({set_owner, Pid}, _From, State) ->
+    {reply, ok, State#state{owner = Pid}};
+
+handle_call(_Request, _From, State) ->
+    {reply, ok, State}.
+
+handle_cast(
+    start_game,
+    #state{
+        socket = Socket
+    } = State
+) ->
+    ok = gen_tcp:send(Socket, [?START_GAME_PACKET(0)]),
+    {noreply, State};
+
+handle_cast(
+    stop_game,
+    #state{
+        socket = Socket
+    } = State
+) ->
+    ok = gen_tcp:send(Socket, [?CANCEL_GAME_PACKET]),
+    {noreply, State};
+
+
+handle_cast(
+    surrender,
+    #state{
+        socket = Socket
+    } = State
+) ->
+    ok = gen_tcp:send(Socket, [?SURRENDER_PACKET(0,0,0)]),
+    {noreply, State};
+
+handle_cast(
+    {make_turn, TurnData},
+    #state{
+        socket = Socket
+    } = State
+) ->
+    ok = gen_tcp:send(Socket, TurnData),
+    {noreply, State};
+
+handle_cast(
+    stop, State
+) ->
+    {noreply, State};
+
+handle_cast(_Request, State) ->
+    {noreply, State}.
+
+handle_info(
+    {tcp, _Socket, Data},
+    #state{
+        parser = Parser
+    } = State
+) ->
+    {ok, NewParser, Messages} = protocol_parser:feed(Parser, Data),
+    lists:foreach(
+        fun(Msg) ->
+            erlang:send(self(), Msg)
+        end, Messages
+    ),
+    {noreply, State#state{
+        parser = NewParser
+    }};
+
+handle_info(
+    {command, ?PING_PACKET = Ping},
+    #state{
+        socket = Socket
+    } = State
+) ->
+    ok = gen_tcp:send(Socket, Ping),
+    {noreply, State};
+
+handle_info(
+    {command, ?SURRENDER_PACKET},
+    #state{
+        owner = Owner
+    } = State
+) ->
+    Owner ! {self(), surrender},
+    {noreply, State};
+
+
+
+handle_info(
+    {command, ?START_GAME_PACKET = StartGame},
+    #state{
+        socket = _Socket,
+        owner = Owner
+    } = State
+) ->
+    OursTurn =
+        case StartGame of
+            ?START_GAME_PACKET(1) -> true;
+            ?START_GAME_PACKET(0) -> false
+        end,
+    ReconnectData =
+        receive
+            {data, Value} ->
+                Value
+        after 1000 ->
+            erlang:error(game_start_fail, "Reconnect data was not received")
+        end,
+    Owner ! {self(), {game_start, OursTurn, ReconnectData}},
+    {noreply, State};
+
+handle_info(
+    {command, ?CANCEL_GAME_PACKET},
+    #state{
+        owner = Owner
+    } = State
+) ->
+    Owner ! {self(), game_stop},
+    {noreply, State};
+
+handle_info(
+    {command, TurnData},
+    #state{
+        owner = Owner
+    } = State
+) ->
+    Owner ! {self(), {turn,TurnData}},
+    {noreply, State};
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
---
+++ apps/inttests/test/integration_tests.erl
@@ -0,0 +1,144 @@
+
+-module(integration_tests).
+-author("Viacheslav V. Kovalev").
+
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("game_server/include/client_protocol.hrl").
+
+before_test() ->
+    %ok = error_logger:tty(false),
+    ok = application:start(game_lobby),
+    ok = application:start(ranch),
+    ok = application:start(game_server).
+
+game_start_stop_test_() ->
+    {setup,
+        fun() ->
+            {ok, FirstEmulator} = client_emulator:start("localhost", 7890),
+            {ok, SecondEmulator} = client_emulator:start("localhost", 7890),
+            {FirstEmulator, SecondEmulator}
+        end,
+        fun({FirstEmulator,SecondEmulator}) ->
+            ok = client_emulator:stop(FirstEmulator),
+            ok = client_emulator:stop(SecondEmulator)
+        end,
+        fun({FirstEmulator,SecondEmulator}) ->
+            {Time, Tests} =
+                timer:tc(
+                    fun() ->
+                        lists:foldl(
+                            fun(_, Acc) ->
+                                ok = client_emulator:start_game(FirstEmulator),
+                                ok = client_emulator:start_game(SecondEmulator),
+                                FirstClientGameStart = lobby_utils:wait_from_pid(FirstEmulator, 100),
+                                SecondClientGameStart = lobby_utils:wait_from_pid(SecondEmulator, 100),
+
+                                ok = client_emulator:stop_game(FirstEmulator),
+                                FirstClientGameStop = lobby_utils:wait_from_pid(FirstEmulator, 100),
+                                SecondClientGameStop = lobby_utils:wait_from_pid(SecondEmulator, 100),
+                                Tests = [
+                                    ?_assertMatch({ok, {game_start, _, _}}, FirstClientGameStart),
+                                    ?_assertMatch({ok, {game_start, _, _}}, SecondClientGameStart),
+                                    ?_assertMatch({ok, game_stop}, FirstClientGameStop),
+                                    ?_assertMatch({ok, game_stop}, SecondClientGameStop)
+                                ],
+                                Acc ++ Tests
+                            end, [], lists:seq(1, 500)
+                        )
+                    end
+                ),
+            ?debugFmt("Total execution time ~p uS",[Time]),
+            Tests
+
+        end
+    }.
+
+game_play_test_() ->
+    {setup,
+        fun() ->
+            {ok, FirstEmulator} = client_emulator:start("localhost", 7890),
+            {ok, SecondEmulator} = client_emulator:start("localhost", 7890),
+            {FirstEmulator, SecondEmulator}
+        end,
+        fun({FirstEmulator,SecondEmulator}) ->
+            ok = client_emulator:stop(FirstEmulator),
+            ok = client_emulator:stop(SecondEmulator)
+        end,
+        fun({FirstEmulator,SecondEmulator}) ->
+            TestHost = self(),
+            PlayFun =
+                fun(Emulator) ->
+                    ok = client_emulator:set_owner(Emulator, self()),
+                    Result = play_game(Emulator, 10),
+                    TestHost ! {self(), Result}
+                end,
+            {Time, Tests} =
+                timer:tc(
+                    fun() ->
+                        lists:foldl(
+                            fun(_, Acc) ->
+                                Player1 = spawn( fun() -> PlayFun(FirstEmulator) end ),
+                                Player2 = spawn( fun() -> PlayFun(SecondEmulator) end ),
+
+                                Result1 = lobby_utils:wait_from_pid(Player1, 1000),
+                                Result2 = lobby_utils:wait_from_pid(Player2, 1000),
+                                Tests = [
+                                    ?_assert(
+                                        (Result1 =:= {ok, win} andalso Result2 =:= {ok, surrender})
+                                            orelse (Result2 =:= {ok, win} andalso Result1 =:= {ok, surrender})
+                                    )
+                                ],
+                                Acc ++ Tests
+                            end, [], lists:seq(1, 500)
+                        )
+                    end
+                ),
+            ?debugFmt("Total execution time ~p uS",[Time]),
+            Tests
+
+        end
+    }.
+
+
+play_game(ClientEmulator, TurnsToMake) ->
+    ok = client_emulator:start_game(ClientEmulator),
+    {ok, {game_start, IsOursTurn, _}} = lobby_utils:wait_from_pid(ClientEmulator, 1000),
+    case IsOursTurn of
+        true ->
+            make_turn(ClientEmulator, TurnsToMake);
+        false ->
+            {ok, {turn, _}} = lobby_utils:wait_from_pid(ClientEmulator, 1000),
+            make_turn(ClientEmulator, TurnsToMake)
+    end.
+
+
+make_turn(ClientEmulator, 0) ->
+    ok = client_emulator:surrender(ClientEmulator),
+    case lobby_utils:wait_from_pid(ClientEmulator, 1000) of
+        {ok, game_stop} ->
+            surrender;
+        _ ->
+            error
+    end;
+make_turn(ClientEmulator, TurnsLeft) ->
+    ok = client_emulator:make_turn(ClientEmulator, <<1,2,3,4>>),
+    case lobby_utils:wait_from_pid(ClientEmulator, 1000) of
+        {ok, surrender} ->
+            ok = client_emulator:surrender(ClientEmulator),
+            case lobby_utils:wait_from_pid(ClientEmulator, 1000) of
+                {ok, game_stop} ->
+                    win;
+                _ ->
+                    error
+            end;
+        {ok, {turn, _}} ->
+            make_turn(ClientEmulator, TurnsLeft-1);
+        _ ->
+            error
+    end.
+
+after_test() ->
+    ok = application:stop(game_server),
+    ok = application:stop(ranch),
+    ok = application:stop(game_lobby).
+    %ok = error_logger:tty(true).
