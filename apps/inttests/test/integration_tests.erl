12f1f59b5c878673944472e633e60e3457a9e106
Viacheslav V. Kovalev <kovyl2404@gmail.com>
2014-07-22 02:38:40 +0400

    Epic refactoring and integration testfixes

--- apps/inttests/test/integration_tests.erl
+++ apps/inttests/test/integration_tests.erl
@@ -6,7 +6,7 @@
 -include_lib("game_server/include/client_protocol.hrl").
 
 before_test() ->
-    %ok = error_logger:tty(false),
+    ok = error_logger:tty(false),
     ok = application:start(game_lobby),
     ok = application:start(ranch),
     ok = application:start(game_server).
@@ -19,7 +19,7 @@ game_reconnect_test_() ->
         fun(_) ->
 
             lists:foldl(
-                fun(I, Acc) ->
+                fun(_I, Acc) ->
                     {ok, TmpFirstEmulator} = client_emulator:start("localhost", 7890),
                     {ok, TmpSecondEmulator} = client_emulator:start("localhost", 7890),
 
@@ -73,93 +73,93 @@ game_reconnect_test_() ->
 
 
 
-%% game_start_stop_test_() ->
-%%     {setup,
-%%         fun() ->
-%%             {ok, FirstEmulator} = client_emulator:start("localhost", 7890),
-%%             {ok, SecondEmulator} = client_emulator:start("localhost", 7890),
-%%             {FirstEmulator, SecondEmulator}
-%%         end,
-%%         fun({FirstEmulator,SecondEmulator}) ->
-%%             ok = client_emulator:stop(FirstEmulator),
-%%             ok = client_emulator:stop(SecondEmulator)
-%%         end,
-%%         fun({FirstEmulator,SecondEmulator}) ->
-%%             {Time, Tests} =
-%%                 timer:tc(
-%%                     fun() ->
-%%                         lists:foldl(
-%%                             fun(_, Acc) ->
-%%                                 ok = client_emulator:start_game(FirstEmulator),
-%%                                 ok = client_emulator:start_game(SecondEmulator),
-%%                                 FirstClientGameStart = lobby_utils:wait_from_pid(FirstEmulator, 100),
-%%                                 SecondClientGameStart = lobby_utils:wait_from_pid(SecondEmulator, 100),
-%%
-%%                                 ok = client_emulator:stop_game(FirstEmulator),
-%%                                 FirstClientGameStop = lobby_utils:wait_from_pid(FirstEmulator, 100),
-%%                                 SecondClientGameStop = lobby_utils:wait_from_pid(SecondEmulator, 100),
-%%                                 Tests = [
-%%                                     ?_assertMatch({ok, {game_start, _, _}}, FirstClientGameStart),
-%%                                     ?_assertMatch({ok, {game_start, _, _}}, SecondClientGameStart),
-%%                                     ?_assertMatch({ok, game_stop}, FirstClientGameStop),
-%%                                     ?_assertMatch({ok, game_stop}, SecondClientGameStop)
-%%                                 ],
-%%                                 Acc ++ Tests
-%%                             end, [], lists:seq(1, 500)
-%%                         )
-%%                     end
-%%                 ),
-%%             ?debugFmt("Total execution time ~p uS",[Time]),
-%%             Tests
-%%
-%%         end
-%%     }.
-%%
-%% game_play_test_() ->
-%%     {setup,
-%%         fun() ->
-%%             {ok, FirstEmulator} = client_emulator:start("localhost", 7890),
-%%             {ok, SecondEmulator} = client_emulator:start("localhost", 7890),
-%%             {FirstEmulator, SecondEmulator}
-%%         end,
-%%         fun({FirstEmulator,SecondEmulator}) ->
-%%             ok = client_emulator:stop(FirstEmulator),
-%%             ok = client_emulator:stop(SecondEmulator)
-%%         end,
-%%         fun({FirstEmulator,SecondEmulator}) ->
-%%             TestHost = self(),
-%%             PlayFun =
-%%                 fun(Emulator) ->
-%%                     ok = client_emulator:set_owner(Emulator, self()),
-%%                     Result = play_game(Emulator, 10),
-%%                     TestHost ! {self(), Result}
-%%                 end,
-%%             {Time, Tests} =
-%%                 timer:tc(
-%%                     fun() ->
-%%                         lists:foldl(
-%%                             fun(_, Acc) ->
-%%                                 Player1 = spawn( fun() -> PlayFun(FirstEmulator) end ),
-%%                                 Player2 = spawn( fun() -> PlayFun(SecondEmulator) end ),
-%%
-%%                                 Result1 = lobby_utils:wait_from_pid(Player1, 1000),
-%%                                 Result2 = lobby_utils:wait_from_pid(Player2, 1000),
-%%                                 Tests = [
-%%                                     ?_assert(
-%%                                         (Result1 =:= {ok, win} andalso Result2 =:= {ok, surrender})
-%%                                             orelse (Result2 =:= {ok, win} andalso Result1 =:= {ok, surrender})
-%%                                     )
-%%                                 ],
-%%                                 Acc ++ Tests
-%%                             end, [], lists:seq(1, 500)
-%%                         )
-%%                     end
-%%                 ),
-%%             ?debugFmt("Total execution time ~p uS",[Time]),
-%%             Tests
-%%
-%%         end
-%%     }.
+game_start_stop_test_() ->
+    {setup,
+        fun() ->
+            {ok, FirstEmulator} = client_emulator:start("localhost", 7890),
+            {ok, SecondEmulator} = client_emulator:start("localhost", 7890),
+            {FirstEmulator, SecondEmulator}
+        end,
+        fun({FirstEmulator,SecondEmulator}) ->
+            ok = client_emulator:stop(FirstEmulator),
+            ok = client_emulator:stop(SecondEmulator)
+        end,
+        fun({FirstEmulator,SecondEmulator}) ->
+            {Time, Tests} =
+                timer:tc(
+                    fun() ->
+                        lists:foldl(
+                            fun(_, Acc) ->
+                                ok = client_emulator:start_game(FirstEmulator),
+                                ok = client_emulator:start_game(SecondEmulator),
+                                FirstClientGameStart = lobby_utils:wait_from_pid(FirstEmulator, 100),
+                                SecondClientGameStart = lobby_utils:wait_from_pid(SecondEmulator, 100),
+
+                                ok = client_emulator:stop_game(FirstEmulator),
+                                FirstClientGameStop = lobby_utils:wait_from_pid(FirstEmulator, 100),
+                                SecondClientGameStop = lobby_utils:wait_from_pid(SecondEmulator, 100),
+                                Tests = [
+                                    ?_assertMatch({ok, {game_start, _, _}}, FirstClientGameStart),
+                                    ?_assertMatch({ok, {game_start, _, _}}, SecondClientGameStart),
+                                    ?_assertMatch({ok, game_stop}, FirstClientGameStop),
+                                    ?_assertMatch({ok, game_stop}, SecondClientGameStop)
+                                ],
+                                Acc ++ Tests
+                            end, [], lists:seq(1, 500)
+                        )
+                    end
+                ),
+            ?debugFmt("Total execution time ~p uS",[Time]),
+            Tests
+
+        end
+    }.
+
+game_play_test_() ->
+    {setup,
+        fun() ->
+            {ok, FirstEmulator} = client_emulator:start("localhost", 7890),
+            {ok, SecondEmulator} = client_emulator:start("localhost", 7890),
+            {FirstEmulator, SecondEmulator}
+        end,
+        fun({FirstEmulator,SecondEmulator}) ->
+            ok = client_emulator:stop(FirstEmulator),
+            ok = client_emulator:stop(SecondEmulator)
+        end,
+        fun({FirstEmulator,SecondEmulator}) ->
+            TestHost = self(),
+            PlayFun =
+                fun(Emulator) ->
+                    ok = client_emulator:set_owner(Emulator, self()),
+                    Result = play_game(Emulator, 10),
+                    TestHost ! {self(), Result}
+                end,
+            {Time, Tests} =
+                timer:tc(
+                    fun() ->
+                        lists:foldl(
+                            fun(_, Acc) ->
+                                Player1 = spawn( fun() -> PlayFun(FirstEmulator) end ),
+                                Player2 = spawn( fun() -> PlayFun(SecondEmulator) end ),
+
+                                Result1 = lobby_utils:wait_from_pid(Player1, 1000),
+                                Result2 = lobby_utils:wait_from_pid(Player2, 1000),
+                                Tests = [
+                                    ?_assert(
+                                        (Result1 =:= {ok, win} andalso Result2 =:= {ok, surrender})
+                                            orelse (Result2 =:= {ok, win} andalso Result1 =:= {ok, surrender})
+                                    )
+                                ],
+                                Acc ++ Tests
+                            end, [], lists:seq(1, 500)
+                        )
+                    end
+                ),
+            ?debugFmt("Total execution time ~p uS",[Time]),
+            Tests
+
+        end
+    }.
 
 
 play_game(ClientEmulator, TurnsToMake) ->
@@ -202,5 +202,5 @@ make_turn(ClientEmulator, TurnsLeft) ->
 after_test() ->
     ok = application:stop(game_server),
     ok = application:stop(ranch),
-    ok = application:stop(game_lobby).
-    %ok = error_logger:tty(true).
+    ok = application:stop(game_lobby),
+    ok = error_logger:tty(true).
